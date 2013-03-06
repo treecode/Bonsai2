8ef4d57e2078ec099e86b007774b0fef6b4e6732
evghenii <egaburov@dds.nl>
2013-03-06 13:47:15 +0100

    added few more fiels and updated Makefile

---
+++ code/computeEnergies.cu
@@ -0,0 +1,10 @@
+#include "Treecode.h"
+
+template<typename real_t, int NLEAF>
+void Treecode<real_t, NLEAF>::computeEnergies()
+{
+  printf("Computing energies\n");
+}
+
+#include "TreecodeInstances.h"
+
---
+++ code/moveParticles.cu
@@ -0,0 +1,10 @@
+#include "Treecode.h"
+
+template<typename real_t, int NLEAF>
+void Treecode<real_t, NLEAF>::moveParticles()
+{
+  printf("move Particles\n");
+}
+
+#include "TreecodeInstances.h"
+
